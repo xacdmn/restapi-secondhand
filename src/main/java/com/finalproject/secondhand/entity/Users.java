56cc9902e9a3a0c30540ddda2fa5926ebc6c8a00
xacdmn <xacdmn@gmail.com>
2022-06-16 20:01:17 +0700

    Initialize Users

---
+++ src/main/java/com/finalproject/secondhand/entity/Users.java
@@ -0,0 +1,52 @@
+package com.finalproject.secondhand.entity;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.Size;
+
+@Setter
+@Getter
+@Entity(name = "users")
+@Table(uniqueConstraints = {
+        @UniqueConstraint(columnNames = "username"),
+        @UniqueConstraint(columnNames = "email")
+})
+
+public class Users {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "username", nullable = false)
+    @Size(max = 30)
+    private String username;
+
+    @Column(name = "email", nullable = false)
+    @Size(max = 30)
+    private String email;
+
+    @Column(name = "password", nullable = false)
+    @Size(min = 8)
+    private String password;
+
+    @Column(name = "address")
+    @Size(max = 200)
+    private String address;
+
+    @Column(name = "phone")
+    @Size(min = 12, max = 13)
+    private Long phoneNumber;
+
+    public Users(String username, String email, String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    public Users() {}
+
+}
