7c834dea39db7cd0df342d14dbd64383fd6da718
xacdmn <xacdmn@gmail.com>
2022-06-17 01:51:31 +0700

    Maintenance Project

--- src/main/java/com/finalproject/secondhand/dto/user/UserUpdateDto.java
+++ src/main/java/com/finalproject/secondhand/dto/user/UserUpdateDto.java
@@ -1,5 +1,14 @@
 package com.finalproject.secondhand.dto.user;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserUpdateDto {
 
     private Integer id;
@@ -7,56 +16,5 @@ public class UserUpdateDto {
     private String password;
     private String address;
     private Long phoneNumber;
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public Long getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(Long phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    public UserUpdateDto (String email, String password, String address, Long phoneNumber) {
-        this.email = email;
-        this.password = password;
-        this.address = address;
-        this.phoneNumber = phoneNumber;
-    }
-
-    public UserUpdateDto() {
-
-    }
 
 }
--- src/main/java/com/finalproject/secondhand/execption/CustomExecption.java
+++
@@ -1,8 +0,0 @@
-package com.finalproject.secondhand.execption;
-
-public class CustomExecption extends IllegalArgumentException {
-    public CustomExecption(String msg) {
-        super(msg);
-    }
-}
-
