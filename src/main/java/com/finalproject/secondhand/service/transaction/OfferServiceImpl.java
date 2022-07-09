ed16d7290dec8c0174b4afbd9ec6b30189d9146e
xacdmn <xacdmn@gmail.com>
2022-07-09 17:55:18 +0700

    [ELLDA] Deployment heroku v1

--- src/main/java/com/finalproject/secondhand/service/transaction/OfferServiceImpl.java
+++ src/main/java/com/finalproject/secondhand/service/transaction/OfferServiceImpl.java
@@ -2,6 +2,7 @@ package com.finalproject.secondhand.service.transaction;
 
 import com.finalproject.secondhand.entity.Offers;
 import com.finalproject.secondhand.entity.Users;
+import com.finalproject.secondhand.enums.EStatusProcess;
 import com.finalproject.secondhand.repository.OfferRepository;
 import com.finalproject.secondhand.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -32,9 +33,13 @@ public class OfferServiceImpl implements OfferService{
     @Override
     public String whatsappSender(Integer offerId) {
         Offers offers = offerRepository.findByOfferId(offerId);
-        String numberPhone = offers.getUsers().getPhone();
-        String apiWhatsapp = "https://api.whatsapp.com/send?phone=";
-        return apiWhatsapp+numberPhone;
+        if (offers.getStatusProcess() == EStatusProcess.ACCEPTED) {
+            String numberPhone = offers.getUsers().getPhone();
+            String apiWhatsapp = "https://api.whatsapp.com/send?phone=";
+            return apiWhatsapp+numberPhone;
+        }else {
+         return "Product not list in status offer accepted";
+        }
     }
 
     @Override
