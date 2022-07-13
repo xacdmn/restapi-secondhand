486581b742a885424809b452698a5e63b106f53d
xacdmn <xacdmn@gmail.com>
2022-07-13 16:48:29 +0700

    [ELLDA] Deployment heroku v1

--- src/main/java/com/finalproject/secondhand/repository/ProductRepository.java
+++ src/main/java/com/finalproject/secondhand/repository/ProductRepository.java
@@ -36,7 +36,7 @@ public interface ProductRepository extends JpaRepository<Products, Integer> {
     Page<Products> findByProductName(String productName, Pageable pageable);
 
     @Query("select  p from products p " +
-            "where p.categories =:categoryId " +
+            "where p.categories.id =:categoryId " +
             "and p.isPublished = true " +
             "and p.isWishlist = false " +
             "and p.isSold = false ")
